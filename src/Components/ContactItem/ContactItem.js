92076162a81f8e521309f84d1170690f845481ed
Svitlana Niesmielova <amelia-com@hotmail.com>
2021-07-20 15:27:21 +0300

    update

--- src/Components/ContactItem/ContactItem.js
+++ src/Components/ContactItem/ContactItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ContactItem = ({ contacts, onDelete }) =>
+const ContactItem = ({ contacts, Delete }) =>
   contacts &&
   contacts.map(({ id, name, number }) => (
     <li key={id}>
